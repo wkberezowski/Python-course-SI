e209bd11e02b87703f71d595e32e1e9b3f565bc0
ZjemCiBabcie <wojtus.berezowski@gmail.com>
2020-05-04 16:01:03 +0200

    added small improvements to the code

--- L5.py
+++ L5.py
@@ -1,23 +1,50 @@
 import requests
 import time
+import sys
 
 
 def max_profit(amount):
     while True:
         list_of_diffs = []
+        all_profit = 0
+
         ETH = requests.get("https://bitbay.net/API/Public/ETH/ticker.json")
         BTC = requests.get("https://bitbay.net/API/Public/BTC/ticker.json")
-        GAME = requests.get("https://bitbay.net/API/Public/GAME/ticker.json")
+        XRP = requests.get("https://bitbay.net/API/Public/XRP/ticker.json")
         LTC = requests.get("https://bitbay.net/API/Public/LTC/ticker.json")
         BCC = requests.get("https://bitbay.net/API/Public/BCC/ticker.json")
 
-        list_of_names = [ETH, BTC, GAME, LTC, BCC]
-        list_of_names_json = [ETH.json(), BTC.json(), GAME.json(), LTC.json(), BCC.json()]
-        list_of_strings = ["ETH", "BTC", "GAME", "LTC", "BCC"]
+        list_of_names_json = [ETH.json(), BTC.json(), XRP.json(), LTC.json(), BCC.json()]
+        list_of_strings = ["ETH", "BTC", "XRP", "LTC", "BCC"]
 
-        for i in list_of_names:
-            formula = round(((i.json()['max'] - i.json()['min']) / i.json()['min']) * 100, 2)
-            list_of_diffs.append(formula)
+        print("Your budget is now {}".format(amount))
+
+        for element in list_of_names_json:
+            if element['volume'] != 0:
+                formula = round(((element['max'] - element['min']) / element['min']) * 100, 2)
+                list_of_diffs.append(formula)
+                name_index = list_of_names_json.index(element)
+                name = list_of_strings[name_index]
+                if amount > 0:
+                    if element['volume'] * element['min'] < amount:
+                        amount -= element['volume'] * element['min']
+                        profit = (element['max'] - element['min']) * element['volume']
+                        all_profit += profit
+                        print("{} of {} can be bought".format(element['volume'] * element['min'], name))
+                        print("Your profit is now {}".format(all_profit))
+                        print("You now have {} USD".format(amount))
+                    else:
+                        profit = (element['max'] - element['min']) * (amount/element['min'])
+                        print("{} of {} can be bought".format(amount / (element['volume'] * element['min']), name))
+                        all_profit += profit
+                        print("Your profit is now {}".format(all_profit))
+                        print("You now have {} USD".format(amount))
+                        amount = 0
+                else:
+                    print("You don't have any money")
+                    sys.exit()
+            else:
+                print("This resource is not available right now")
 
         for k in range(len(list_of_diffs)):
             if list_of_diffs[k] > 0:
@@ -27,19 +54,8 @@ def max_profit(amount):
             else:
                 print("{} {}%".format(list_of_strings[k], list_of_diffs[k]))
 
-        for element in list_of_names_json:
-            if amount > 0:
-                if element['volume'] * element['min'] < amount:
-                    amount -= element['volume'] * element['min']
-                    name_index = list_of_names_json.index(element)
-                    name = list_of_strings[name_index]
-                    print("{} of {} can be bought".format(element['volume'] * element['min'], name))
-                    print("You now have {} USD".format(amount))
-            elif amount == 0:
-                print("You don't have any money left")
-
         print(40*"=")
         time.sleep(300)
 
 
-max_profit(10000)
+max_profit(200000)
